a242785af556c0d7aa40acfa3590e14990022ced
manh.nguyen.dev <manh.mobile.dev@gmail.com>
2025-05-30 22:34:29 +0700

    chore(ComingSoonScreen): update placeholder text

--- lib/core/ui/coming_soon_screen.dart
+++ lib/core/ui/coming_soon_screen.dart
@@ -42,17 +42,16 @@ class ComingSoonScreen extends StatelessWidget {
                 ),
                 const SizedBox(height: 24),
                 const Text(
-                  'COMING SOON',
+                  'Tính năng sẽ sớm có mặt',
                   style: TextStyle(
-                    fontSize: 32,
+                    fontSize: 26,
                     fontWeight: FontWeight.bold,
-                    letterSpacing: 2,
                     color: Colors.deepPurple,
                   ),
                 ),
                 const SizedBox(height: 12),
                 const Text(
-                  'We’re working hard to bring you this feature in Memory Notes. Stay tuned!',
+                  'Chúng tôi đang tích cực phát triển tính năng này trên Memory Notes. Vui lòng chờ đợi bản cập nhật tiếp theo!',
                   textAlign: TextAlign.center,
                   style: TextStyle(fontSize: 16, color: Colors.black87),
                 ),
